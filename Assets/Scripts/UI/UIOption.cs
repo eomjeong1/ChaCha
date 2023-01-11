167b81f1e115ac341b4743341f9bf037d4bcac77
JYS121206 <p.uncia1775@gmail.com>
2023-01-11 18:34:25 +0900

    isCor[스테이지넘버]값 저장

--- Assets/Scripts/UI/UIOption.cs
+++ Assets/Scripts/UI/UIOption.cs
@@ -12,10 +12,12 @@ public class UIOption : MonoBehaviour
     public Image imgCor;
     public Image imgIncor;
 
+    int curcor;
     ScenesManager scenesManager;
 
     void Start()
     {
+        curcor = ScenesManager.GetInstance().currentGame - 1;
         scenesManager = ScenesManager.GetInstance();
 
         //ScenesManager.GetInstance().currentGame = 5; //테스트용//
@@ -38,7 +40,8 @@ public class UIOption : MonoBehaviour
         GameManager.GetInstance().curScore--;
         Debug.Log($"틀렸습니다. / 남은 사과: {GameManager.GetInstance().curScore}개");
         scenesManager.isCorr = false;
-        scenesManager.isCor[ScenesManager.GetInstance().currentGame] = false;
+        scenesManager.isCor[curcor] = false;
+        Debug.Log($"답 기록: {scenesManager.isCor[curcor]}");
         ScenesManager.GetInstance().ChangeScene(Scene.Changer2);
 
     }
@@ -47,7 +50,8 @@ public class UIOption : MonoBehaviour
     {
         Debug.Log($"정답입니다. / 남은 사과: {GameManager.GetInstance().curScore}개");
         scenesManager.isCorr = true;
-        scenesManager.isCor[ScenesManager.GetInstance().currentGame] = true;
+        scenesManager.isCor[curcor] = true;
+        Debug.Log($"답 기록: {scenesManager.isCor[curcor]}");
         ScenesManager.GetInstance().ChangeScene(Scene.Changer2);
 
     }
