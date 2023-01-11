3f952476a0f27a7bbf67a4eb2ae9090e9ac75b14
JYS121206 <p.uncia1775@gmail.com>
2023-01-11 17:34:08 +0900

    누락된 스크립트 재업로드

---
+++ Assets/Scripts/UI/UIOption.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIOption : MonoBehaviour
+{
+    public Button cor;
+    public Button incor;
+
+    public Image imgCor;
+    public Image imgIncor;
+
+
+    void Start()
+    {
+        //ScenesManager.GetInstance().currentGame = 5; //테스트용//
+        SetOptionSc();
+        cor.onClick.AddListener(Corr);
+        incor.onClick.AddListener(Incorr);
+
+
+        gameObject.SetActive(false); //테스트 끝나면 켜세요//
+    }
+
+    void SetOptionSc()
+    {
+        imgCor.sprite = Resources.Load<Sprite>($"Image/Stage{ScenesManager.GetInstance().currentGame}/Cor");
+        imgIncor.sprite = Resources.Load<Sprite>($"Image/Stage{ScenesManager.GetInstance().currentGame}/Incor");
+    }
+
+    public void Incorr()
+    {
+        GameManager.GetInstance().curScore--;
+        Debug.Log($"틀렸습니다. / 남은 사과: {GameManager.GetInstance().curScore}개");
+        ScenesManager.GetInstance().isCorr = false;
+        ScenesManager.GetInstance().ChangeScene(Scene.Changer2);
+
+    }
+
+    public void Corr()
+    {
+        Debug.Log($"정답입니다. / 남은 사과: {GameManager.GetInstance().curScore}개");
+        ScenesManager.GetInstance().isCorr = true;
+        ScenesManager.GetInstance().ChangeScene(Scene.Changer2);
+
+    }
+  
+}
