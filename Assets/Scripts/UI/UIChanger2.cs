05845b5d0db959cedfe567fce1a9f1ebe4cb4e3e
JYS121206 <p.uncia1775@gmail.com>
2023-01-11 17:35:38 +0900

    Revert "Revert "누락된 스크립트 재업로드""
    This reverts commit 40b1e139e46970c1802a384112be2665d192a7be.

---
+++ Assets/Scripts/UI/UIChanger2.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class UIChanger2 : MonoBehaviour
+{
+    public VideoPlayer vid;
+    bool isCorr;
+    int currentGame;
+    public string curScene;
+
+    public Button btnSkip;
+
+    void Start()
+    {
+        this.isCorr = ScenesManager.GetInstance().isCorr;
+        this.currentGame = ScenesManager.GetInstance().currentGame;
+
+        btnSkip.onClick.AddListener(OnSkip);
+        vid = GetComponentInChildren<VideoPlayer>();
+
+        if (isCorr)
+            curScene = $"corr{currentGame}";
+
+        else
+            curScene = $"incor{currentGame}";
+
+
+        vid.clip = Resources.Load<VideoClip>($"Video/corr/{curScene}");
+
+        Debug.Log($"영상이 종료된 후 맵으로 이동합니다.");
+
+        vid.loopPointReached += CheckOver;
+    }
+
+    void CheckOver(UnityEngine.Video.VideoPlayer vp)
+    {
+        Debug.Log("맵");
+        ScenesManager.GetInstance().ChangeScene(Scene.Changer1);
+
+        ScenesManager.GetInstance().currentGame++;
+    }
+
+    void OnSkip()
+    {
+        ScenesManager.GetInstance().ChangeScene(Scene.Changer1);
+        ScenesManager.GetInstance().currentGame++;
+    }
+}
