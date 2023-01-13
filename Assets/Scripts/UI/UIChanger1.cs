ad392c247d5ddfab00246a3ff7222a9f482b9d11
JYS121206 <p.uncia1775@gmail.com>
2023-01-13 10:47:12 +0900

    스킵버튼 비활성화

--- Assets/Scripts/UI/UIChanger1.cs
+++ Assets/Scripts/UI/UIChanger1.cs
@@ -17,7 +17,7 @@ public class UIChanger1 : MonoBehaviour
         this.currentGame = ScenesManager.GetInstance().currentGame;
 
         btnSkip.onClick.AddListener(OnSkip);
-        //btnSkip.gameObject.SetActive(false);
+        btnSkip.gameObject.SetActive(false);
         vid = GetComponentInChildren<VideoPlayer>();
 
         curScene = $"Game{currentGame}";
